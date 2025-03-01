2a83ef8e864c8f4a2379d55ea3cb3adf7c641114
s0nprem0 <s0nprem0@proton.me>
2025-03-01 18:13:22 +0800

    chore(nvim): update ui.lua

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -87,36 +87,6 @@ return {
   },
 
   -- filename
-  {
-    "b0o/incline.nvim",
-    dependencies = { "craftzdog/solarized-osaka.nvim" },
-    event = "BufReadPre",
-    priority = 1200,
-    config = function()
-      local colors = require("solarized-osaka.colors").setup()
-      require("incline").setup({
-        highlight = {
-          groups = {
-            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
-            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
-          },
-        },
-        window = { margin = { vertical = 0, horizontal = 1 } },
-        hide = {
-          cursorline = true,
-        },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[+] " .. filename
-          end
-
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-          return { { icon, guifg = color }, { " " }, { filename } }
-        end,
-      })
-    end,
-  },
 
   -- statusline
   {
@@ -161,14 +131,20 @@ return {
       dashboard = {
         preset = {
           header = [[
-	        ██████╗ ███████╗██╗   ██╗ █████╗ ███████╗██╗     ██╗███████╗███████╗
-	        ██╔══██╗██╔════╝██║   ██║██╔══██╗██╔════╝██║     ██║██╔════╝██╔════╝
-	        ██║  ██║█████╗  ██║   ██║███████║███████╗██║     ██║█████╗  █████╗
-	        ██║  ██║██╔══╝  ╚██╗ ██╔╝██╔══██║╚════██║██║     ██║██╔══╝  ██╔══╝
-	        ██████╔╝███████╗ ╚████╔╝ ██║  ██║███████║███████╗██║██║     ███████╗
-	        ╚═════╝ ╚══════╝  ╚═══╝  ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝     ╚══════╝
-   ]],
+          I miss you
+          ]],
         },
+      },
+      sections = {
+        section = "terminal",
+        cmd = "chafa ~/.config/wall.png --format symbols --symbols --vhalf --size 60x17 --stretech; sleep .1",
+        height = 17,
+        padding = 1,
+      },
+      {
+        pane = 2,
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
       },
     },
   },
