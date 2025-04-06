b5ddc86ee1bd113b829402dc498787dd7016096d
s0nprem0 <s0nprem0@proton.me>
2025-04-06 23:14:09 +0800

    chore(nvim): update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,39 +3,49 @@
 -- Add any additional options here
 
 vim.g.mapleader = " "
-
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 vim.opt.number = true
 
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-vim.opt.hlsearch = true
-vim.opt.backup = false
-vim.opt.showcmd = true
-vim.opt.cmdheight = 0
-vim.opt.laststatus = 0
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.inccommand = "split"
-vim.opt.ignorecase = true
-vim.opt.smarttab = true
 vim.opt.breakindent = true
+
+vim.opt.expandtab = true -- convert tabs to backspace
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.wrap = false
+vim.opt.softtabstop = 2
+
+vim.opt.hlsearch = true
+vim.opt.backup = false
+
+vim.opt.cmdheight = 0
+vim.opt.laststatus = 0
+vim.opt.scrolloff = 10
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+
+
+
 vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.path:append({ "**" })
 vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.splitkeep = "cursor"
-vim.opt.mouse = ""
+vim.opt.mouse = "a"
 vim.wo.number = true
-
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
-
