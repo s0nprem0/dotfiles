c71cdd7e10681a7ff386f08e58d7aa61ccf3aa44
s0nprem0 <s0nprem0@proton.me>
2025-04-12 21:06:43 +0800

    chore(nvim): update various plugins

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -6,16 +6,33 @@ return {
     priority = 1000,
     config = function()
       require("catppuccin").setup({
-        flavour = "macchiato",
+        flavour = "macchiato", -- latte, frappe, macchiato, mocha
+        background = { -- h background
+          light = "latte",
+          dark = "macchiato",
+        },
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15,
+        },
         transparent_background = true,
         term_colors = true,
         styles = {
           comments = { "italic" },
           conditionals = { "italic" },
         },
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       })
       -- Set the theme
-      vim.cmd.colorscheme "catppuccin"
-    end
-  }
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
