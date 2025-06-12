8d0d6d7b42ca33d9bb842701a22fc022928d066e
s0nprem0 <s0nprem0@proton.me>
2025-06-12 15:11:27 +0800

    arch btw

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
-#
+#!/bin/bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
- polybar
+polybar -r mybar &
