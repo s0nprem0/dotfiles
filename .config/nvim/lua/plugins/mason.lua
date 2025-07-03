33a3418f1b8f7c170464ecfbee197461d2e86ac1
s0nprem0 <s0nprem0@proton.me>
2025-07-03 16:24:15 +0800

    chore(nvim): update

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,45 @@
 return {
-  "mason-org/mason.nvim",
-  opts = {},
+  "williamboman/mason.nvim",
+  dependencies = {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "williamboman/mason-lspconfig.nvim", -- Recommended for LSP integration
+  },
+  opts = {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+    },
+  },
+  config = function(_, opts)
+    require("mason").setup(opts)
+
+    -- Tool installer setup
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        -- LSPs
+        "bash-language-server",
+        "jdtls",
+        "lua_ls",
+        "pyright",
+
+        -- Formatters (choose one of prettier/prettierd)
+        "prettier",
+        "stylua",
+
+        -- Linters
+        "shellcheck",
+        "eslint_d",
+      },
+      auto_update = true,
+      run_on_start = true, -- Set to false if startup is too slow
+    })
+
+    -- Recommended: LSP config setup
+    require("mason-lspconfig").setup({
+      automatic_installation = true, -- Auto-install LSPs listed in lspconfig
+    })
+  end,
 }
