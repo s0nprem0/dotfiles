1dbd9a334c04a01801e117f63f5ab058609f40cd
s0nprem0 <s0nprem0@proton.me>
2026-06-13 15:53:34 +0800

    refactor(zsh): improve vcs_info prompt performance

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,31 +1,45 @@
-autoload -Uz colors && colors
-autoload -Uz vcs_info
+autoload -Uz colors vcs_info add-zsh-hook
+colors
 
-zstyle ':vcs_info:git:*' formats '(%b)'
-zstyle ':vcs_info:git:*' actionformats '(%b|%a)'
+# Git configuration
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+
+zstyle ':vcs_info:git:*' unstagedstr '*'
+zstyle ':vcs_info:git:*' stagedstr '+'
+
+zstyle ':vcs_info:git:*' formats '(%b%c%u)'
+zstyle ':vcs_info:git:*' actionformats '(%b|%a%c%u)'
 
 PROMPT_ALTERNATIVE=twoline
 
 configure_prompt() {
-    local venv='${VIRTUAL_ENV:+($(basename "$VIRTUAL_ENV"))}'
-    local chroot='${debian_chroot:+($debian_chroot)}'
+    local venv='${VIRTUAL_ENV:+(${VIRTUAL_ENV:t})}'
 
     case "$PROMPT_ALTERNATIVE" in
         twoline)
-            PROMPT=$'%F{green}┌──'"$chroot$venv"$'──(%B%F{blue}%n@%m%b%f)-[%B%4~%b]\n└─%(#.%F{red}#.%F{blue}$)%f '
+            PROMPT=$'%F{green}┌──'
+            PROMPT+="$venv"
+            PROMPT+=$'──(%B%F{blue}%n@%m%b%f)-[%B%4~%b]\n'
+            PROMPT+=$'└─%(#.%B%F{red}#%b.%F{blue}$)%f '
             RPROMPT='%F{cyan}${vcs_info_msg_0_}%f'
             ;;
         oneline)
-            PROMPT="$chroot$venv"$'%B%F{blue}%n@%m%b%f:%B%F{green}%~%b%f %(#.#.$) '
+            PROMPT="$venv"
+            PROMPT+=$'%B%F{blue}%n@%m%b%f:'
+            PROMPT+=$'%B%F{green}%4~%b%f '
+            PROMPT+=$'%(#.%B%F{red}#%b.%F{blue}$)%f '
             RPROMPT='%F{cyan}${vcs_info_msg_0_}%f'
             ;;
     esac
 }
 
 toggle_oneline_prompt() {
-    [[ "$PROMPT_ALTERNATIVE" == oneline ]] &&
-        PROMPT_ALTERNATIVE=twoline ||
+    if [[ "$PROMPT_ALTERNATIVE" == oneline ]]; then
+        PROMPT_ALTERNATIVE=twoline
+    else
         PROMPT_ALTERNATIVE=oneline
+    fi
 
     configure_prompt
     zle reset-prompt
@@ -34,5 +48,7 @@ toggle_oneline_prompt() {
 zle -N toggle_oneline_prompt
 bindkey '^[p' toggle_oneline_prompt
 
-precmd() { vcs_info }
+add-zsh-hook precmd vcs_info
+
 configure_prompt
+
