b653316b02e3099a47816233959daa89164393cc
s0nprem0 <s0nprem0@proton.me>
2026-06-24 08:43:31 +0800

    fix(osd): invert Caps/Num lock state and use full path for osdctl

--- .config/quickshell/helpers/lock_osd.sh
+++ .config/quickshell/helpers/lock_osd.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-# ; ponytail: reads lock key state from xset, shows OSD
+# Reads lock key state from xset, inverts it (bind fires before toggle), shows OSD
+dir=$(dirname "$0")
 state=$(xset -q | sed -n 's/^.*'"$1"': *//p' | awk '{print $1}')
-osdctl show "$1: $state" info 1800
+case "$state" in
+  on)  new="off" ;;
+  off) new="on"  ;;
+  *)   new="$state" ;;
+esac
+"$dir/osdctl" show "$1: $new" info 1800
